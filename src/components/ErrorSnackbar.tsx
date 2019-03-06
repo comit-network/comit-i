46676ac3abde14bc6bb10f9a779efbaa2a2d5cb1
Thomas Eizinger <thomas@eizinger.io>
2019-03-06 14:50:42 +1100

    Make ErrorSnackbar not go away on clicking and make it red

--- src/components/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar.tsx
@@ -1,16 +1,21 @@
-import { IconButton, Snackbar, SnackbarContent } from "@material-ui/core";
+import {
+  createStyles,
+  IconButton,
+  Snackbar,
+  SnackbarContent,
+  Theme,
+  Typography,
+  WithStyles,
+  withStyles
+} from "@material-ui/core";
 import React from "react";
 import CloseIcon from "@material-ui/icons/Close";
 import ErrorIcon from "@material-ui/icons/Error";
-import { makeStyles } from "@material-ui/styles";
 
-interface ErrorSnackbarProps {
-  open: boolean;
-  onClose: () => void;
-  message: string;
-}
-
-const useStyles = makeStyles(theme => ({
+const styles = (theme: Theme) => createStyles({
+  content: {
+    backgroundColor: theme.palette.error.dark,
+  },
   message: {
     display: "flex",
     alignItems: "center"
@@ -18,10 +23,15 @@ const useStyles = makeStyles(theme => ({
   icon: {
     marginRight: theme.spacing.unit
   }
-}));
+});
 
-function ErrorSnackbar({ open, onClose, message }: ErrorSnackbarProps) {
-  const classes = useStyles();
+interface ErrorSnackbarProps extends WithStyles<typeof styles> {
+  open: boolean;
+  onClose: () => void;
+  message: string;
+}
+
+function ErrorSnackbar({ open, onClose, message, classes }: ErrorSnackbarProps) {
 
   return (
     <Snackbar
@@ -31,15 +41,19 @@ function ErrorSnackbar({ open, onClose, message }: ErrorSnackbarProps) {
       }}
       open={open}
       onClose={onClose}
+      ClickAwayListenerProps={{
+        onClickAway: () => {}
+      }}
     >
       <SnackbarContent
         classes={{
+          root: classes.content,
           message: classes.message
         }}
         message={
           <React.Fragment>
             <ErrorIcon className={classes.icon} />
-            <span>{message}</span>
+            <Typography color={"inherit"}>{message}</Typography>
           </React.Fragment>
         }
         action={[
@@ -57,4 +71,4 @@ function ErrorSnackbar({ open, onClose, message }: ErrorSnackbarProps) {
   );
 }
 
-export default ErrorSnackbar;
+export default withStyles(styles)(ErrorSnackbar);
