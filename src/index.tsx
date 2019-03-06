45aebc571ff65401e7d9cd2b205814d8defaa0d2
Thomas Eizinger <thomas@eizinger.io>
2019-03-06 16:48:26 +1100

    Automate UI tests for travis configuration

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+// Not all environments support Promise.finally
+import "promise-polyfill/src/polyfill";
+
 ReactDOM.render(<App />, document.getElementById("root"));
