36e8edc2b8d6193771303c9a4c67a4bf058d2897
Thomas Eizinger <thomas@eizinger.io>
2019-03-11 11:35:51 +1100

    Convert navigation bar to permanent left drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,115 @@
-import { createMuiTheme } from "@material-ui/core";
+import {
+  AppBar,
+  createMuiTheme,
+  createStyles,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Theme,
+  Toolbar,
+  Typography,
+  WithStyles,
+  withStyles
+} from "@material-ui/core";
+import InboxIcon from "@material-ui/icons/Inbox";
+import SendIcon from "@material-ui/icons/Send";
 import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import TopAppBar from "./components/TopAppBar";
+import LinkPlus from "./components/LinkPlus";
 import ListSwaps from "./pages/ListSwaps/ListSwaps";
 
-const theme = createMuiTheme({
+const appTheme = createMuiTheme({
   typography: {
     useNextVariants: true
   }
 });
 
-const App = () => (
-  <ThemeProvider theme={theme}>
+const drawerWidth = 240;
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex"
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3
+    },
+    toolbar: theme.mixins.toolbar,
+    grow: {
+      flexGrow: 1
+    }
+  });
+
+interface AppProps extends WithStyles<typeof styles> {}
+
+const App = ({ classes }: AppProps) => (
+  <ThemeProvider theme={appTheme}>
     <BrowserRouter>
-      <React.Fragment>
-        <TopAppBar />
-        <MainContent />
-      </React.Fragment>
+      <div className={classes.root}>
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              COMIT-i
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          anchor="left"
+          classes={{
+            paper: classes.drawerPaper
+          }}
+        >
+          <div className={classes.toolbar} />
+          <Divider />
+          <List>
+            <ListItem button={true} key={"swaps"}>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Swaps"} />
+            </ListItem>
+
+            <ListItem button={true} key={"new_swap_link"}>
+              <ListItemIcon>
+                <LinkPlus />
+              </ListItemIcon>
+              <ListItemText primary={"Create swap link"} />
+            </ListItem>
+
+            <ListItem button={true} key={"send_swap"}>
+              <ListItemIcon>
+                <SendIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Send swap request"} />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Route exact={true} path="/" component={ListSwaps} />
+          <Route path="/make_link" component={CreateNewSwap} />
+          <Route path="/new_swap" component={LinkLandingPage} />
+        </main>
+      </div>
     </BrowserRouter>
   </ThemeProvider>
-);
-
-const MainContent = () => (
-  <React.Fragment>
-    <Route exact={true} path="/" component={ListSwaps} />
-    <Route path="/make_link" component={CreateNewSwap} />
-    <Route path="/new_swap" component={LinkLandingPage} />
-  </React.Fragment>
 );
 
 const CreateNewSwap = () => <div>Here is where you can create a new swap</div>;
@@ -35,4 +117,4 @@ const LinkLandingPage = () => (
   <div>Here is where you land when you click on a swap-link</div>
 );
 
-export default App;
+export default withStyles(styles)(App);
