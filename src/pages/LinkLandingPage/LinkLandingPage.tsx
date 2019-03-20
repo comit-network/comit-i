f7f0c7d8ea73f9ce4ad7b31326daf0a696d0fc0d
Philipp Hoenisch <philipp@hoenisch.at>
2019-03-20 15:36:56 +1100

    Add try catch to parsing methods

--- src/pages/LinkLandingPage/LinkLandingPage.tsx
+++ src/pages/LinkLandingPage/LinkLandingPage.tsx
@@ -1,4 +1,4 @@
-import { createStyles, Typography } from "@material-ui/core";
+import { createStyles, Paper, Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/styles";
 import { WithStyles } from "@material-ui/styles/withStyles";
 import queryString, { ParsedQuery } from "query-string";
@@ -56,47 +56,45 @@ interface LinkLandingProps
     WithStyles<typeof styles> {}
 
 function parseLedger(
-  input: string | string[] | undefined | null
-): Ledger | null {
+  input: string | string[] | undefined | null,
+  ledger: string
+): Ledger {
   if (input instanceof Array) {
     input = input[0];
   }
   if (!input) {
-    return null;
+    throw new Error(ledger + " undefined");
   }
   const regex = /([A-Za-z0-9]+)(-([a-zA-Z0-9]+))?/g;
 
   const match = regex.exec(input) || [];
-  if (!match.length) {
-    return null;
+  if (!match.length || !match[1]) {
+    throw new Error(ledger + " ledger could not be parsed. Was: " + input);
   }
-  /* eslint-disable no-console */
-  // console.log(match);
-  /* eslint-enable no-console */
-  const name = match[1] || "";
+  const name = match[1];
   const network = match[3] || "mainnet";
   return { name, network };
 }
 
-function parseAsset(input: string | string[] | undefined | null): Asset | null {
+function parseAsset(
+  input: string | string[] | undefined | null,
+  asset: string
+): Asset {
   if (input instanceof Array) {
     input = input[0];
   }
   if (!input) {
-    return null;
+    throw new Error(asset + " undefined");
   }
   const regex = /([.0-9]+)([a-zA-Z]+[0-9]*)(:(.+))?/g;
 
   const match = regex.exec(input) || [];
-  if (!match.length) {
-    return null;
+  if (!match.length || !match[1] || !match[2]) {
+    throw new Error(asset + " asset could not be parsed. Was: " + input);
   }
-  /* eslint-disable no-console */
-  // console.log(match);
-  /* eslint-enable no-console */
-  const amount = match[1] || "";
-  const symbol = match[2] || "";
-  const tokenId = match[4] || "";
+  const amount = match[1];
+  const symbol = match[2];
+  const tokenId = match[4] || undefined;
 
   return { amount, symbol, tokenId };
 }
@@ -115,20 +113,16 @@ function parseProtocol(protocol: string | string[] | undefined | null) {
   return protocol || "";
 }
 
-function parseSwapParams(parsedQuery: ParsedQuery): SwapParams | null {
-  const alphaLedger = parseLedger(parsedQuery.alpha_ledger);
-  const alphaAsset = parseAsset(parsedQuery.alpha_asset);
+function parseSwapParams(parsedQuery: ParsedQuery): SwapParams {
+  const alphaLedger = parseLedger(parsedQuery.alpha_ledger, "alpha");
+  const alphaAsset = parseAsset(parsedQuery.alpha_asset, "alpha");
 
-  const betaLedger = parseLedger(parsedQuery.beta_ledger);
-  const betaAsset = parseAsset(parsedQuery.beta_asset);
+  const betaLedger = parseLedger(parsedQuery.beta_ledger, "beta");
+  const betaAsset = parseAsset(parsedQuery.beta_asset, "beta");
 
   const protocol = parseProtocol(parsedQuery.protocol);
   const peer = parsePeer(parsedQuery.peer);
   const id = "42";
-
-  if (!alphaAsset || !alphaLedger || !betaAsset || !betaLedger) {
-    return null;
-  }
 
   return {
     alphaLedger,
@@ -142,44 +136,58 @@ function parseSwapParams(parsedQuery: ParsedQuery): SwapParams | null {
 }
 
 const LinkLandingPage = ({ location }: LinkLandingProps) => {
-  const swapParams = parseSwapParams(queryString.parse(location.search));
-  /* eslint-disable no-console */
-  // console.log(swapParams);
-  /* eslint-enable no-console */
+  let swapParams;
+  let showError = false;
+  let errorMessage = "";
+  try {
+    swapParams = parseSwapParams(queryString.parse(location.search));
+  } catch (error) {
+    /* eslint-disable no-console */
+    // console.log(error);
+    /* eslint-enable no-console */
+    showError = true;
+    errorMessage = error.toString();
+  }
 
   return (
     <React.Fragment>
-      <fieldset>
-        <legend>Link Parameters</legend>
-        <Typography variant="body1">
-          The swap you select, sorry but you can't change it:
-        </Typography>
-        {swapParams && (
-          <Typography variant="body2">
-            Alpha Ledger: {swapParams.alphaLedger.name} on{" "}
-            {swapParams.alphaLedger.network}
-            <br />
-            Alpha Asset {swapParams.alphaAsset.amount}{" "}
-            {swapParams.alphaAsset.symbol}
-            {swapParams.alphaAsset.tokenId &&
-              "Token: " + swapParams.alphaAsset.tokenId}
-            <br />
-            Beta Ledger: {swapParams.betaLedger.name} on{" "}
-            {swapParams.betaLedger.network}
-            <br />
-            Beta Asset {swapParams.betaAsset.amount}{" "}
-            {swapParams.betaAsset.symbol}
-            {swapParams.betaAsset.tokenId &&
-              " Token: " + swapParams.betaAsset.tokenId}
-            <br />
-            Peer: {swapParams.peer} <br />
-            Protocol: {swapParams.protocol} <br />
-            Trade ID: {swapParams.id} <br />
-          </Typography>
-        )}
-      </fieldset>
+      <Paper elevation={1}>
+        <fieldset>
+          <legend>Link Parameters</legend>
+          {swapParams && (
+            <Typography variant="body1">
+              The swap you selected, sorry but you can't change it:
+            </Typography>
+          )}
+          {swapParams && (
+            <Typography variant="body2">
+              Alpha Ledger: {swapParams.alphaLedger.name} on{" "}
+              {swapParams.alphaLedger.network}
+              <br />
+              Alpha Asset {swapParams.alphaAsset.amount}{" "}
+              {swapParams.alphaAsset.symbol}
+              {swapParams.alphaAsset.tokenId &&
+                "Token: " + swapParams.alphaAsset.tokenId}
+              <br />
+              Beta Ledger: {swapParams.betaLedger.name} on{" "}
+              {swapParams.betaLedger.network}
+              <br />
+              Beta Asset {swapParams.betaAsset.amount}{" "}
+              {swapParams.betaAsset.symbol}
+              {swapParams.betaAsset.tokenId &&
+                " Token: " + swapParams.betaAsset.tokenId}
+              <br />
+              Peer: {swapParams.peer} <br />
+              Protocol: {swapParams.protocol} <br />
+              Trade ID: {swapParams.id} <br />
+            </Typography>
+          )}
 
-      {swapParams && <LinkLandingForm swapParams={swapParams} />}
+          {showError && <div className="error-message">{errorMessage}</div>}
+        </fieldset>
+
+        {swapParams && <LinkLandingForm swapParams={swapParams} />}
+      </Paper>
     </React.Fragment>
   );
 };
