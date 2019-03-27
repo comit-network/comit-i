e148c0d95cecfedbdfc4e92af10e23b6a2803080
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-03-27 16:44:40 +1100

    Improve settings
    - Create interface to handle `localStorage`.
    - Add `SuccessSnackbar`. Together with `ErrorSnackbar` it should be
      generalised into a `Snackbar` component.

---
+++ src/components/SuccessSnackbar.tsx
@@ -0,0 +1,80 @@
+// TODO: Create generic Snackbar component out of this and ErrorSnackbar
+import {
+  createStyles,
+  IconButton,
+  Snackbar,
+  SnackbarContent,
+  Theme,
+  Typography,
+  WithStyles,
+  withStyles
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import DoneIcon from "@material-ui/icons/Done";
+import React from "react";
+
+const styles = (theme: Theme) =>
+  createStyles({
+    content: {
+      backgroundColor: theme.palette.primary.dark
+    },
+    message: {
+      display: "flex",
+      alignItems: "center"
+    },
+    icon: {
+      marginRight: theme.spacing.unit
+    }
+  });
+
+interface SuccessSnackbarProps extends WithStyles<typeof styles> {
+  open: boolean;
+  onClose: () => void;
+  message: string;
+}
+
+function SuccessSnackbar({
+  open,
+  onClose,
+  message,
+  classes
+}: SuccessSnackbarProps) {
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left"
+      }}
+      open={open}
+      onClose={onClose}
+      ClickAwayListenerProps={{
+        onClickAway: () => {} // tslint:disable-line no-empty
+      }}
+    >
+      <SnackbarContent
+        classes={{
+          root: classes.content,
+          message: classes.message
+        }}
+        message={
+          <React.Fragment>
+            <DoneIcon className={classes.icon} />
+            <Typography color={"inherit"}>{message}</Typography>
+          </React.Fragment>
+        }
+        action={[
+          <IconButton
+            key="close"
+            aria-label="Close"
+            color="inherit"
+            onClick={onClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        ]}
+      />
+    </Snackbar>
+  );
+}
+
+export default withStyles(styles)(SuccessSnackbar);
--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,10 +1,13 @@
-import { Button, Grid, TextField } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 import SaveIcon from "@material-ui/icons/Save";
 import { makeStyles } from "@material-ui/styles";
 import { useState } from "react";
 import React from "react";
 import Fieldset from "../../components/Fieldset";
 import Page from "../../components/Page";
+import SuccessSnackbar from "../../components/SuccessSnackbar";
+import TextField from "../../components/TextField";
+import storage from "../../storage";
 
 const useSettingsStyles = makeStyles(theme => ({
   button: {
@@ -16,44 +19,63 @@ const useSettingsStyles = makeStyles(theme => ({
 }));
 
 function Settings() {
-  const [hostAndPort, setHostAndPort] = useState(
-    localStorage.getItem("hostAndPort") || "localhost:8000"
-  );
+  const [host, setHost] = useState(storage.getHost());
+  const [port, setPort] = useState(storage.getPort());
+  const [displaySaved, setDisplaySaved] = useState(false);
 
   const classes = useSettingsStyles();
 
-  const handleSaveSettings = () => {
-    localStorage.setItem("hostAndPort", hostAndPort);
+  const handleSaveSettings = (event: any) => {
+    event.preventDefault();
+
+    storage.setHost(host);
+    storage.setPort(port);
+
+    setDisplaySaved(true);
+    setTimeout(() => setDisplaySaved(false), 2500);
   };
 
   return (
-    <Page title="Set your settings">
-      <form onSubmit={handleSaveSettings}>
-        <Grid container={true} spacing={40}>
-          <Grid item={true} xs={12}>
-            <Fieldset legend="COMIT node">
-              <TextField
-                label="Host and port"
-                value={hostAndPort}
-                onChange={event => setHostAndPort(event.target.value)}
-                SelectProps={{
-                  native: true
-                }}
-              />
-            </Fieldset>
-          </Grid>
-        </Grid>
-        <Button
-          variant="contained"
-          type="submit"
-          color="primary"
-          className={classes.button}
-        >
-          Save
-          <SaveIcon className={classes.icon} />
-        </Button>
-      </form>
-    </Page>
+    <React.Fragment>
+      <Page title="Set your settings">
+        <form onSubmit={handleSaveSettings}>
+          <Fieldset legend="COMIT node">
+            <Grid container={true} spacing={40}>
+              <Grid item={true} xs={12} md={6}>
+                <TextField
+                  label="Host"
+                  value={host}
+                  onChange={event => setHost(event.target.value)}
+                />
+              </Grid>
+              <Grid item={true} xs={12} md={6}>
+                <TextField
+                  label="Port"
+                  value={port}
+                  onChange={event => setPort(parseInt(event.target.value, 10))}
+                />
+              </Grid>
+            </Grid>
+          </Fieldset>
+          <Button
+            variant="contained"
+            type="submit"
+            color="primary"
+            className={classes.button}
+          >
+            Save
+            <SaveIcon className={classes.icon} />
+          </Button>
+        </form>
+      </Page>
+      {displaySaved && (
+        <SuccessSnackbar
+          message={"Your settings have been saved."}
+          onClose={() => setDisplaySaved(false)}
+          open={true}
+        />
+      )}
+    </React.Fragment>
   );
 }
 
