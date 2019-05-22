b697c27acf8e51f2ccdb0258fc9d3911220adf95
Franck Royer <franck@coblox.tech>
2019-05-22 13:10:00 +1000

    Load config from comit_node
    load the connection details config from comit_node and uses it if
    it is available. Default to storage if not.

--- src/api/apiEndpoint.ts
+++ src/api/apiEndpoint.ts
@@ -2,6 +2,18 @@ import URI from "urijs";
 import storage from "../storage";
 
 export default function apiEndpoint() {
+  if (window.getComitNodeApiEndpoint) {
+    const { host, port } = window.getComitNodeApiEndpoint();
+
+    if (host && port) {
+      return new URI({
+        protocol: "http",
+        hostname: host,
+        port: port.toString()
+      });
+    }
+  }
+
   return new URI({
     protocol: "http",
     hostname: storage.getHost(),
