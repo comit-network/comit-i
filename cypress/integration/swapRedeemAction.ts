12c8de72ec44f54b355ab39fdf4281d6c4eb0d4a
Thomas Eizinger <thomas@eizinger.io>
2019-05-30 18:09:55 +1000

    Add UI tests for actions

--- cypress/integration/swapRedeemAction.ts
+++ cypress/integration/swapRedeemAction.ts
@@ -6,12 +6,12 @@ describe("The Redeem action", () => {
       cy.server();
       cy.route(
         "http://localhost:8000/swaps",
-        "fixture:swapRedeemEthereum.json"
+        "fixture:swapWithRedeemEthereum.json"
       );
       cy.route({
         method: "GET",
         url: "**/redeem",
-        response: "fixture:ethereumCallContractLedgerAction.json",
+        response: "fixture:ledgerActions/ethereum-call-contract.json",
         headers: {
           "content-type": "application/json"
         },
@@ -30,5 +30,47 @@ describe("The Redeem action", () => {
     });
   });
 
-  // describe("Bitcoin redeem action", () => {});
+  describe("Bitcoin redeem action", () => {
+    it("should show a ledger action to the user in a dialog", () => {
+      cy.server();
+      cy.route(
+        "http://localhost:8000/swaps",
+        "fixture:swapWithRedeemBitcoin.json"
+      );
+      cy.route({
+        method: "GET",
+        url: "**/redeem?**",
+        response:
+          "fixture:ledgerActions/bitcoin-broadcast-signed-transaction.json",
+        headers: {
+          "content-type": "application/json"
+        },
+        delay: 2000
+      }).as("getLedgerAction");
+
+      cy.visit("/show_resource/swaps");
+      cy.get("[data-cy=redeem-button]").click();
+
+      cy.get("[data-cy=dialog]").should("exist");
+      cy.get("[data-cy=dialog]")
+        .find("[data-cy=action-text-field]:nth(0)")
+        .find("input")
+        .type("bitcoin_address");
+
+      cy.get("[data-cy=dialog]")
+        .find("[data-cy=action-text-field]:nth(1)")
+        .find("input")
+        .type(20);
+
+      cy.get("[data-cy=dialog]")
+        .find("[data-cy=redeem-button]")
+        .click();
+      cy.get("[data-cy=dialog]").should("not.exist");
+
+      cy.get("[data-cy=action-request-circular-progress]").should("exist");
+      cy.wait("@getLedgerAction");
+
+      cy.get("[data-cy=dialog]").should("exist");
+    });
+  });
 });
