6627d8e563e9587cbd10f3a7f32172fe8f0a8f3f
da-kami <d.karzel@gmail.com>
2019-06-04 15:08:27 +1000

    update stories to material-ui version
    * fix story problems related to new material-ui version
    * fix warning

--- stories/EthereumAddressTextField.tsx
+++ stories/EthereumAddressTextField.tsx
@@ -1,13 +1,16 @@
 import { withState } from "@dump247/storybook-state";
+import {ThemeProvider} from "@material-ui/styles";
 import { storiesOf } from "@storybook/react";
 import React from "react";
 import EthereumAddressTextField from "../src/components/EthereumAddressTextField";
 import { Web3Provider } from "../src/components/Web3Context";
+import appTheme from "../src/theme";
 
 storiesOf("EthereumAddressTextField", module)
   .add(
     "default",
     withState({ address: "" })(({ store }) => (
+        <ThemeProvider theme={appTheme}>
       <Web3Provider>
         <EthereumAddressTextField
           value={store.state.address}
@@ -17,5 +20,6 @@ storiesOf("EthereumAddressTextField", module)
           }}
         />
       </Web3Provider>
+        </ThemeProvider>
     ))
   );
