b01a3d80d61377053ee03b9162cff5c50da272c7
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-06-07 15:46:12 +0200

    Optimise shortened swap ID generation

--- src/pages/SwapList/SwapRow/SwapId.tsx
+++ src/pages/SwapList/SwapRow/SwapId.tsx
@@ -7,12 +7,7 @@ interface SwapIdProps {
 
 export default function SwapId({ id }: SwapIdProps) {
   const firstThree = id.slice(0, 3);
-  const lastThree = id
-    .split("")
-    .reverse()
-    .slice(0, 3)
-    .reverse()
-    .join("");
+  const lastThree = id.substring(id.length - 3);
 
   return (
     <Tooltip title={id}>
