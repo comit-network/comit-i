24375d83ebeaad0421255c824043998e2254f8af
Lucas Soriano del Pino <l.soriano.del.pino@gmail.com>
2019-08-14 11:27:11 +1000

    Fix boolean logic bug
    The desired effect is to disable the automatic reload of resources
    when any action dialog is open.

--- src/pages/actions/useAllowReload.tsx
+++ src/pages/actions/useAllowReload.tsx
@@ -8,6 +8,6 @@ export default function useAllowReload(
   setAllowReload: (arg: boolean) => void
 ) {
   useEffect(() => {
-    setAllowReload(!!activeLedgerActionDialog || !!activeSirenParameterDialog);
+    setAllowReload(!activeLedgerActionDialog && !activeSirenParameterDialog);
   }, [activeLedgerActionDialog, activeSirenParameterDialog, setAllowReload]);
 }
